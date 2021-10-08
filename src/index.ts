e33264bcb07060a8e7d87951eacb6b49d2185a99
Ratna Lama <royalray7@gmail.com>
2021-10-07 20:33:02 -0700

    added basic number types and array types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-let id:number = 5;
+// let id: number = 5;
 
-console.log(id); // 5
+// console.log(id); // 5
+
+/**
+ * Basic Types
+ * 
+ * - number
+ * - string
+ * - boolean
+ * - any (number, string, boolean)
+ * - array of type string/number/boolean/any
+ * 
+ */
+
+let id: number = 5;
+let company: string = 'Hello World!';
+let isPublished: boolean = true;
+let x: any = 'Ola!'
+
+// Array of type number
+let count: number[]  = [0, 1,2,3,4,5,6,7]
+count = [100]
+// Array of type string
+let words: string[] =['alpha', 'beta', 'gamma']
+words = ['delta']
+// Array of type any
+let mix: any[] = [0, 1, 'Hello', true]
+mix = [123.4567]
+
+// Tupble
