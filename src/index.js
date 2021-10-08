d741c06bb0ceacf4f34d18e896230260b88b53a9
Ratna Lama <royalray7@gmail.com>
2021-10-07 20:32:37 -0700

    updated index.js file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
+// let id: number = 5;
+// console.log(id); // 5
+/**
+ * Basic Types
+ *
+ * - number
+ * - string
+ * - boolean
+ * - any (number, string, boolean)
+ * - array of type string/number/boolean/any
+ *
+ */
 var id = 5;
-console.log(id); // 5
+var company = 'Hello World!';
+var isPublished = true;
+var x = 'Ola!';
+// Array of type number
+var count = [0, 1, 2, 3, 4, 5, 6, 7];
+count = [100];
+// Array of type string
+var words = ['alpha', 'beta', 'gamma'];
+words = ['delta'];
+// Array of type any
+var mix = [0, 1, 'Hello', true];
+mix = [123.4567];
+// Tupble
